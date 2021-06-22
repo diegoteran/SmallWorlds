91095be82f94d893bf517bc0b16472fdea0a2b6b
Diego Terán Ríos <diegoteran94@gmail.com>
2021-06-22 14:37:29 -0700

    World Particles Fix

---
+++ SmallWorlds/World/WorldParticle.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+export(float) var lifetime = 7.0
+export(float) var lifetime_variation = 5.0
+
+onready var particle = $Particles2D
+onready var timer = $Timer
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	timer.connect("timeout", self, "on_Timer_timeout")
+	timer.start(randf() * 5)
+
+
+func on_Timer_timeout():
+	if Globals.dead == false:
+		global_position = Globals.player.global_position
+	var new_lifetime = lifetime + randf() * lifetime_variation
+	particle.lifetime = new_lifetime
+	particle.restart()
+	timer.start(new_lifetime)
---
+++ SmallWorlds/World/WorldParticles.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+export var WorldParticle: PackedScene
+
+export(int) var particle_num = 15
+var particles = []
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in range(particle_num):
+		var new_particle = Globals.instance_scene_on_node(WorldParticle, self, global_position)
+		particles.append(new_particle)
+
+
+func queue_free():
+	for particle in particles:
+		particle.queue_free()
+		.queue_free()
