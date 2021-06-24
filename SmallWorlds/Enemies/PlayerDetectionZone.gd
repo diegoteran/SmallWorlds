b89914e7bd57ea8506e07adc943f147cfa6afe49
Diego Terán Ríos <diegoteran94@gmail.com>
2021-06-24 01:13:00 -0700

    StingFly + Update player detection to support multiple players

--- SmallWorlds/Enemies/PlayerDetectionZone.gd
+++ SmallWorlds/Enemies/PlayerDetectionZone.gd
@@ -1,12 +1,22 @@
 extends Area2D
 
+var players = {}
 var player = null
 
 func can_see_player():
-	return player != null
+	return players.size() > 0
 
 func _on_PlayerDetectionZone_body_entered(body):
-	player = body
+	players[body.name] = body
+	choose_new_player()
 
-func _on_PlayerDetectionZone_body_exited(_body):
-	player = null
+func _on_PlayerDetectionZone_body_exited(body):
+	players.erase(body.name)
+	if players.size() > 0:
+		choose_new_player()
+	else:
+		player = null
+
+func choose_new_player():
+	var player_key = players.keys()[randi() % players.size()]
+	player = players[player_key]
